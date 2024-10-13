6d19efd6b35c65320d4a527beefc9f4f6460b6be
patrickverol <patrickverol@gmail.com>
2024-10-12 22:23:38 -0300

    update EC2 Flask

--- IaC/iac_deploy/ec2_flask.tf
+++ IaC/iac_deploy/ec2_flask.tf
@@ -6,18 +6,6 @@ resource "aws_instance" "ec2_flask" {
   iam_instance_profile = aws_iam_instance_profile.ec2_s3_profile_v2.name
   depends_on = [aws_s3_bucket.project_ml_bucket]
 
-  # Startup script
-  user_data = <<-EOF
-                #!/bin/bash
-                sudo yum update -y
-                sudo yum install -yp python3 python3-pip awscli
-                sudo mkdir /frontend
-                sudo aws s3 sync s3://ml-api-369199697991-bucket-ml-files/frontend /frontend
-                cd /frontend
-                sudo pip3 install flask python-dotenv gunicorn httpx
-                nohup gunicorn -w 4 -b 0.0.0.0:5000 flask_app:app &
-              EOF
-
   tags = {
     ProjectName = var.project_name
     Label       = var.label
